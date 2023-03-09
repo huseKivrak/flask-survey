9afec4f050c8c710ac5b8c38e7627f929d1a078d
William Squires <william.squiresii@gmail.com>
2023-03-09 14:26:49 -0600

    we can ask a single question.

--- app.py
+++ app.py
@@ -7,3 +7,26 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def show_start_page():
+    """Show the begin page for a survey. Redirects to the first question"""
+
+    return render_template("survey_start.html", s=survey)
+
+@app.post("/begin")
+def redirect_to_first_question():
+    """When a survey is started, redirect the user to the first question"""
+
+    question_number = len(responses)
+    return redirect(f"/questions/{question_number}")
+
+
+@app.get("/questions/<int:question_number>")
+def show_question(question_number):
+    """Shows the current question for the survey."""
+
+    q = survey.questions[question_number]
+    return render_template("question.html", question=q)
