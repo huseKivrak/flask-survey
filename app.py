dce8af89e6c3ac497bbd072899e3078672a908b1
William Squires <william.squiresii@gmail.com>
2023-03-09 20:01:50 -0600

    we use session instead of global variables now

--- app.py
+++ app.py
@@ -8,14 +8,12 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
+#responses = []
 
 
 @app.get("/")
 def show_start_page():
     """Show the begin page for a survey."""
-
- 
 
     return render_template("survey_start.html", survey=survey)
 
@@ -25,14 +23,27 @@ def redirect_to_first_question():
     """When a survey is started, redirect the user to the first question"""
 
     # makes sure our responses are empty before we start the survey
-    responses.clear()
-    
-    return redirect(f"/questions/0")
+    #responses.clear()
+
+    session["responses"] = []
+    session["existence"] = True
+
+    return redirect("/questions/0")
 
 
 @app.get("/questions/<int:question_number>")
 def show_question(question_number):
-    """Shows the current question for the survey."""
+    """Validates we are allowed to be at this question.
+    Shows the current question for the survey if we are allowed.
+    Otherwise, send us to the right question, or home if there is no session
+    """
+
+    if not session.get("existence"):
+        return redirect("/")
+    
+    num_responses = len(session["responses"])
+    if num_responses != question_number:
+        return redirect(f"/questions/{num_responses}")
 
     question = survey.questions[question_number]
     return render_template("question.html", question=question)
@@ -46,11 +57,14 @@ def get_answer_and_redirect():
 
     answer = request.form["answer"]
 
-    responses.append(answer)
+    # can't append directly to the session, so we make a temp list to append to
+    temp_responses = session["responses"]
+    temp_responses.append(answer)
+    session["responses"] = temp_responses
 
-    question_number = len(responses)
+    question_number = len(session["responses"])
 
-    if len(responses) == len(survey.questions):
+    if len(session["responses"]) == len(survey.questions):
         return redirect("/thankyou")
 
     return redirect(f"/questions/{question_number}")
@@ -64,6 +78,6 @@ def show_thankyou_page():
 
     prompts = [question.prompt for question in questions]
 
-    completed_survey = [item for item in zip(prompts, responses)]
+    completed_survey = [item for item in zip(prompts, session["responses"])]
     return render_template("completion.html",
                            completed_survey=completed_survey)
